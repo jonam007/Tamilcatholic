8a0b11621edd18fea28efa862bdd5b102e3e8fdd
Manoj Kumar Ku <63900409+jonam007@users.noreply.github.com>
2025-08-12 02:45:28 +0000

    initial commit

--- server/config/passport.js
+++ server/config/passport.js
@@ -1,7 +1,8 @@
 import passport from 'passport';
 import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
 import User from '../models/User.js';
-
+import dotenv from 'dotenv';
+dotenv.config();
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
